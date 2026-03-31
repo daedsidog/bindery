5f68020af1c90a94a4a9ba6d0e54b1e06cbfd4e0
daedsidog <41439659+daedsidog@users.noreply.github.com>
2026-04-01 02:31:20 +0300

    Replace external registry with Pathway workspace

--- source/package.lisp
+++ source/package.lisp
@@ -1,21 +1,6 @@
-;;;; Copyright (C) 2025 DAEDSIDOG.  All rights reserved.
-
-(uiop:define-package #:cfli
-  (:use #:clean)
-  (:reexport #:cfli/external-registry))
+(defpackage #:cfli
+  (:use #:clean))
 
 (in-package #:cfli)
 
-(defclass asdf-user::cxx-grovel-file (cffi-grovel:grovel-file) ())
-
-#+win32
-(defmethod asdf:perform :around ((op cffi-grovel::process-op) (c asdf-user::cxx-grovel-file))
-  (let ((old-value cffi-toolchain:*ld-exe-flags*))
-    (setf cffi-toolchain:*ld-exe-flags*
-          (substitute "-Wl,--export-all-symbols"
-                      "-Wl,--export-dynamic"
-                      old-value
-                      :test #'string=))
-    (unwind-protect
-        (call-next-method)
-      (setf cffi-toolchain:*ld-exe-flags* old-value))))
+(pushnew '(pathway:default-workspace-pathname) cffi:*foreign-library-directories* :test #'equalp)
